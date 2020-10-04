97e203565327930294ec1c5685beb7fd4fff93de
ednese <evancours94@gmail.com>
2020-10-05 00:08:43 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '@/App'
+Vue.use(Router)
 
-// Vue.use(Router)
-
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'HelloWorld',
-//       component: HelloWorld
-//     }
-//   ]
-// })
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'App',
+      component: App
+    }
+  ]
+})
